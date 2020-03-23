8c0709d38d19958a9eb85756876608d5acd0e690
rloganiv <rloganiv@gmail.com>
2020-03-23 12:37:32 -0700

    Added metadata to reader + GPT2 specific tests.

--- acceptability/data/dataset_readers/prefix_lm.py
+++ acceptability/data/dataset_readers/prefix_lm.py
@@ -6,12 +6,11 @@ import numpy as np
 from overrides import overrides
 
 from allennlp.data.dataset_readers import DatasetReader
-from allennlp.data.fields import Field, ArrayField, TextField
+from allennlp.data.fields import Field, ArrayField, MetadataField, TextField
 from allennlp.data.instance import Instance
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Token, Tokenizer
 from allennlp.data.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
-
 
 logger = logging.getLogger(__name__)
 
@@ -32,14 +31,20 @@ class PrefixLmReader(DatasetReader):
     def text_to_instance(
         self,
         prefix: str,
-        suffix: str
+        suffix: str,
     ) -> Instance:
-        prefix_tokens = self._tokenizer.tokenize(prefix)
-        suffix_tokens = self._tokenizer.tokenize(suffix)
+        # HuggingFace's tokenizers require leading whitespace.
+        prefix_tokens = self._tokenizer.tokenize(' ' + prefix)
+        suffix_tokens = self._tokenizer.tokenize(' ' + suffix)
         tokens = prefix_tokens + suffix_tokens
         eval_mask = np.array([0] * len(prefix_tokens) + [1] * len(suffix_tokens))
+        metadata = {
+            'prefix': [t.text for t in prefix_tokens],
+            'suffix': [t.text for t in suffix_tokens],
+        }
         fields = {
             'tokens': TextField(tokens, token_indexers=self._token_indexers),
-            'eval_mask': ArrayField(eval_mask, dtype=bool)
+            'eval_mask': ArrayField(eval_mask, dtype=bool),
+            'metadata': MetadataField(metadata),
         }
         return Instance(fields)
--- acceptability/tests/data/dataset_readers/test_prefix_lm.py
+++ acceptability/tests/data/dataset_readers/test_prefix_lm.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from allennlp.data.tokenizers import PretrainedTransformerTokenizer
+from allennlp.data.token_indexers import PretrainedTransformerIndexer
 from allennlp.data.vocabulary import Vocabulary
 
 from acceptability.data.dataset_readers import PrefixLmReader
@@ -23,14 +25,47 @@ class TestPrefixLmReader(TestCase):
         instance.index_fields(vocab)
         tensor_dict = instance.as_tensor_dict(instance.get_padding_lengths())
 
-        # Check that default tokenizer works as expected
         tokens = instance['tokens']
-        self.assertListEqual([t.text for t in tokens], ['This', 'is', 'a', 'difficult', 'test'])
+        self.assertListEqual(
+            [t.text for t in tokens],
+            ['This', 'is', 'a', 'difficult', 'test']
+        )
 
-        # Check that default token_indexer works as expected
         token_ids = tensor_dict['tokens']['tokens']['tokens']
         self.assertListEqual(token_ids.tolist(), [2, 3, 4, 5, 6])
 
-        # Check that eval mask works as expected
         eval_mask = tensor_dict['eval_mask']
         self.assertListEqual(eval_mask.tolist(), [0, 0, 0, 1, 1])
+
+        metadata = tensor_dict['metadata']
+        self.assertListEqual(metadata['prefix'],  ['This', 'is', 'a'])
+        self.assertListEqual(metadata['suffix'],  ['difficult', 'test'])
+
+    def test_gpt2_text_to_instance(self):
+        tokenizer = PretrainedTransformerTokenizer(
+            model_name='gpt2',
+            add_special_tokens=False
+        )
+        token_indexers={
+            'tokens': PretrainedTransformerIndexer(model_name='gpt2')
+        }
+        reader = PrefixLmReader(
+            tokenizer=tokenizer,
+            token_indexers=token_indexers
+        )
+        instance = reader.text_to_instance(prefix='This is a', suffix='difficult test')
+        instance.index_fields(Vocabulary())
+        tensor_dict = instance.as_tensor_dict(instance.get_padding_lengths())
+
+        tokens = instance['tokens']
+        self.assertListEqual(
+            [t.text for t in tokens],
+            ['ĠThis', 'Ġis', 'Ġa', 'Ġdifficult', 'Ġtest']
+        )
+
+        eval_mask = tensor_dict['eval_mask']
+        self.assertListEqual(eval_mask.tolist(), [0, 0, 0, 1, 1])
+
+        metadata = tensor_dict['metadata']
+        self.assertListEqual(metadata['prefix'], ['ĠThis', 'Ġis', 'Ġa'])
+        self.assertListEqual(metadata['suffix'], ['Ġdifficult', 'Ġtest'])
